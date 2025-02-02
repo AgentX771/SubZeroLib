fb35ecf05411c62f235b5b5ee499e546399c382e
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-02-01 18:55:17 -0600

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,13 +1,1 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-local playerNamesToCheck = {"ZxxxZ2422", "OtroJugador", "MasJugadores"}
-
-for _, playerName in ipairs(playerNamesToCheck) do
-    if LocalPlayer and LocalPlayer.Name == playerName then
-        if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
-            LocalPlayer:Destroy()
-            break
-        end
-    end
-end
+local Blacklist = {"ZxxxZ242", "PlayerZxxxZ"}
