e654e0875eed00ccfb9218f7d17dc95847deaf70
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-02-15 22:02:58 -0600

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,1 +1,1 @@
-local Blacklist = {"thevinhctvn372013", "missaelinsanox", "anxinngheokhoqua", "tocinacio_242", "aaskibi7"}
+local Blacklist = {"thevinhctvn372013", "missaelinsanox", "anxinngheokhoqua", "tocinacio_242", "aaskibi7", "urgurl2451"}
