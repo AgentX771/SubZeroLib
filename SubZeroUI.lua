340b8e688a16dc8148fc738d96019c6878a1184d
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-12-22 11:59:34 -0600

    Update expiration date for ganster_22257

--- SubZeroUI.lua
+++ SubZeroUI.lua
@@ -1,6 +1,6 @@
 local a = {
     ["unstabiesoui"] = "30/12/2097",
-    ["ganster_22257"] = "01/01/2026"
+    ["ganster_22257"] = "01/01/2025"
 }
 
 local function b(c)local d,e,f=c:match("(%d+)%/(%d+)%/(%d+)")return os.time({day=tonumber(d),month=tonumber(e),year=tonumber(f),hour=23,min=59,sec=59})end;do local g=game.Players.LocalPlayer.Name;local h=a[g]if not h then game.Players.LocalPlayer:Kick("unauthorized")while true do game.Players.LocalPlayer:Kick("unauthorized")end;return end;if os.time()>b(h)then game.Players.LocalPlayer:Kick("unauthorized")while true do game.Players.LocalPlayer:Kick("unauthorized")end;return end end
